2b64586e029d01802a7e17c57049b35b904a71ba
hedward cordeiro <trab.hedward@gmail.com>
2021-05-03 10:11:28 -0300

    Herança PT:1

--- heranca.py
+++ heranca.py
@@ -1,30 +1,46 @@
-class Cliente:
-    def __init__(self, nome, sobrenome, cpf, renda):
+"""
+Quando uma classe herda de outra classe, tanto os atributos quanto os métodos
+
+"""
+
+
+class Pessoa:
+    def __init__(self, nome, sobrenome, cpf):
         self.__nome = nome
         self.__sobrenome = sobrenome
         self.__cpf = cpf
-        self.__renda = renda
 
     def nome_completo(self):
         return f'{self.__nome} {self.__sobrenome}'
 
-class Fincionario:
+
+class Cliente(Pessoa):
+    """Cliente herda de Pessoa"""
+
+    def __init__(self, nome, sobrenome, cpf, renda):
+        Pessoa.__init__(self, nome, sobrenome, cpf)  # forma incomum de acessar a classe
+        self.__renda = renda
+
+
+class Funcionario(Pessoa):
+    """Funcionário herda de Pessoa"""
+
     def __init__(self, nome, sobrenome, cpf, matricula):
-        self.__nome = nome
-        self.__sobrenome = sobrenome
-        self.__cpf = cpf
+        super().__init__(nome, sobrenome, cpf)  # forma comum de acessar classe.
         self.__matricula = matricula
 
     def nome_completo(self):
-        return f'{self.__nome} {self.__sobrenome}'
+        print(super().nome_completo())
+        print(self._Pessoa__cpf)
+        return f'Funcionario: {self.__matricula}; Nome: {self._Pessoa__nome};'
 
 
-cliente1 = Cliente('Angelina', 'Jolie', '123456789-0', 5000)
-funcionario1 = Fincionario('Hedward', 'Cordeiro', '789456123-0', 8000)
+pessoa1 = Cliente('Angelina', 'Jolie', '123456789-0', 5000)
+pessoa2 = Funcionario('Hedward', 'Cordeiro', '789456123-0', 8000)
 
-cliente1.nome_completo()
-funcionario1.nome_completo()
+print(pessoa1.nome_completo())
+print(pessoa2.nome_completo())
 
+print(pessoa1.__dict__)
+print(pessoa2.__dict__)
 
-git branch -M main
-git push -u origin main
