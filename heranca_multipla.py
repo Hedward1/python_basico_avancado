9aff0f58fa9a92d3342c8d864da1e4b8483d20bd
hedward <trab.hedward@gmail.com>
2021-05-06 08:53:31 -0300

    Primeiro commit Herança Múltipla

---
+++ heranca_multipla.py
@@ -0,0 +1,48 @@
+"""
+PODE SER FEITA DE DUAS MANEIRAS:
+    1 - MULTIDERIVAÇÃO DIRETA.
+    2 - MULTIDERIVAÇÃO INDIRETA.
+
+
+
+class Base1:
+    pass
+
+
+class Base2:
+    pass
+
+
+class Base3:
+    pass
+
+
+class MultiDerivada(Base1, Base2, Base3):
+    pass
+
+
+# exemplo indireta
+
+
+class Base1:
+    pass
+
+
+class Base2(Base1):
+    pass
+
+
+class Base3(Base2):
+    pass
+
+
+class MultiDerivada(Base3):
+    pass
+
+
+"""
+
+
+
+
+
